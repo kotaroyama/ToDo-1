fbec12f263ff16ab34ae8a4d28035a5ff9a17fb9
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-03-18 23:47:24 +0800

    Edit models.py schemas.py

--- models.py
+++ models.py
@@ -3,5 +3,6 @@ from sqlmodel import SQLModel, Field
 
 class Task(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    title: str
+    title: str = Field(max_length=100, index=True)
     description: str | None = None
+    completed: bool = Field(default=False)
--- schemas.py
+++ schemas.py
@@ -4,3 +4,4 @@ from sqlmodel import SQLModel
 class TaskUpdate(SQLModel):
     title: str | None = None
     description: str | None = None
+    completed: str | None = None
