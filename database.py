eb3d91a5c1707962a4414c271bd9df1292d64e07
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-03-21 19:08:43 +0800

    Switch from SQLite to PostgreSQL

--- database.py
+++ database.py
@@ -1,12 +1,7 @@
 from sqlmodel import SQLModel, Session, create_engine
 
-sqlite_url = "sqlite:///database.db"
-engine = create_engine(
-    sqlite_url,
-    connect_args={
-        "check_same_thread": False
-    },
-)
+DATABASE_URL = "postgresql://kotaro@localhost:5432/todo"
+engine = create_engine(DATABASE_URL)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
