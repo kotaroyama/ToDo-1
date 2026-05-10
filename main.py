cd5be2ec44b78029708edb9841e34461a6a571cd
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-05-11 02:31:57 +0800

    Add CORS

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://todo1-fastapi.com", "http://localhost:5173"],
+    allow_origins=["https://todo1-fastapi.com", "http://localhost:5173", "http://localhost"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
